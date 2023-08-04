d3ab71424ad62b0eb04fb2d88d4bd6ca1798a4ae
Nikhil <nikhil.t@msr-it.com>
2023-08-04 20:42:36 +0530

    first commit

---
+++ src/main/java/com/javatechie/azure/SpringAzureDemooApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAzureDemooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureDemooApplication.class, args);
+	}
+
+}
